f0a5cff1c95f74cd114951d5ef0dec8b80cfc031
denis <denis.baklykov@boxberry.ru>
2022-08-26 22:29:43 +0300

    получение данных с openweathermap

--- src/index.js
+++ src/index.js
@@ -57,7 +57,15 @@ const dataWeather = {
 async function drawWeather(el, data) {
   const response = await fetch("https://get.geojs.io/v1/ip/geo.json");
   const json = await response.json();
-  el.innerHTML = `<h1>${json.region}</h1><p>${data.main.temp}</p>
+
+  const responseOpenweathermap = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${json.latitude}&lon=${json.longitude}&appid=2dd5152e26591562500eba5a006f9a67`
+  );
+  const jsonOpenweathermap = await responseOpenweathermap.json();
+
+  const temp = (jsonOpenweathermap.main.temp - 273.15).toFixed(0);
+
+  el.innerHTML = `<h1>${json.region}</h1><p>${temp}&deg;</p>
                    <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">`;
 }
 
